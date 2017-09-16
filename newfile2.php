a298161d56ea122352c6d6f04fe0be6d83042867
xabertum <mohatar@gmail.com>
2017-09-16 11:22:17 +0200

    Added new file

---
+++ newfile2.php
@@ -0,0 +1,20 @@
+<?php
+
+$favcolor = "red";
+
+switch ($favcolor) {
+	case "red" :
+		echo "Your favorite color is red";
+		break;
+	case "blue":
+		echo "Your favorite color is Blue";
+		break;
+	case "green": 
+		echo "Your favourite color is green!";
+		break;
+	default:
+		echo "Your favourite color is neither above";
+	
+}
+
+?>
